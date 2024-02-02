d654c1c8e99aa63197c2a2983c3229b1803e7412
jochterski-gwc <157849715+jochterski-gwc@users.noreply.github.com>
2024-02-02 08:58:06 -0500

    New repository

---
+++ script.js
@@ -0,0 +1,54 @@
+/*
+KEY FEATURE: Personalize it
+https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null
+*/
+
+var displayScript = document.getElementById("scriptReturned");
+var scriptBtn = document.getElementById("scriptBtn");
+//Will need to explain that sometimes the DOM doesn't load all the way, so the browser can't find the element for JavaScript to use
+
+if (scriptBtn) {
+  scriptBtn.addEventListener("click", generateScript);
+}
+
+/*
+REFACTOR ITEM 2: 
+Create a separate function for displaying the script.
+Be sure to update function names.
+*/
+
+function generateScript() {
+  //This variable must be in the function or it will not work
+  var name = document.getElementById("name").value;
+  var location = document.getElementById("location").value;
+  var learning = document.getElementById("learning").value;
+
+  displayScript.innerHTML = "Hello, my name is " + name + " and I live in " + location + ". I am calling to ask you to support H.R.4229 Broadband Deployment Accuracy and Technological Availability (DATA) Act for more accurate broadband maps. As a student, my learning now depends on my access to the internet. For example, " + learning + ". Please support H.R.4229 Broadband Deployment Accuracy and Technological Availability (DATA) Act for more accurate broadband maps.";
+}
+
+
+//Move these variables to the top
+var factList = [
+  "16.9 million children lack the high-speed home internet access necessary to support online learning", 
+  "7.3 million children do not have a desktop, laptop, or tablet computer", 
+  "One in three Black, Latino, and American Indian/Alaska Native families do not have high-speed home internet", 
+  "One in three families who earn less than $50,000 annually do not have high-speed home internet", 
+  "Two in five families in rural areas do not have high-speed home internet", 
+  "12 million kids live in homes without a broadband Internet connection"
+];
+
+var fact = document.getElementById("fact");
+var factBtn = document.getElementById("factBtn");
+var count = 0;
+
+if (factBtn) {
+  factBtn.addEventListener("click", displayFact);
+}
+
+function displayFact() {
+  fact.innerHTML = factList[count];
+  count++;
+  if (count == factList.length) {
+    count = 0;
+  }
+}
